6750276e222d7894cf1c773751d2a2f9b4292c9a
Harshith N J <njharshith122@gmail.com>
2025-03-31 15:21:39 +0530

    Save a parking spot data

--- src/main/java/org/parking/parking_spot/controller/parkingSpotController.java
+++ src/main/java/org/parking/parking_spot/controller/parkingSpotController.java
@@ -1,7 +1,11 @@
 package org.parking.parking_spot.controller;
 
+import org.parking.parking_spot.dto.parkingSpot;
 import org.parking.parking_spot.service.parkingSpotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,11 @@ public class parkingSpotController {
     
     @Autowired
     parkingSpotService service;
+
+
+    //Post the parking spot data
+    @PostMapping("/parkingSpots")
+    public ResponseEntity<Object> postParkingSpot(@RequestBody parkingSpot spot){
+        return service.postParkingSpot(spot);
+    }
 }
--- src/main/java/org/parking/parking_spot/service/parkingSpotService.java
+++ src/main/java/org/parking/parking_spot/service/parkingSpotService.java
@@ -1,7 +1,13 @@
 package org.parking.parking_spot.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.parking.parking_spot.dto.parkingSpot;
 import org.parking.parking_spot.repository.parkingSpotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +15,22 @@ public class parkingSpotService {
     
     @Autowired
     parkingSpotRepository repository;
+
+    public ResponseEntity<Object> postParkingSpot(parkingSpot spot) {
+
+        if(repository.ExistsBySpotNumber(spot.getSpotNumber())) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("Error", "Spot already exists");
+
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        } else {
+            repository.save(spot);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("Success", "Spot added successfully");
+            map.put("Parking Spot", spot);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
 }
